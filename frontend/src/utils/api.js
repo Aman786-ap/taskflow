77db1981e4dba03ef119d83554696311ed7cfa95
Aman <ap9091645@gmail.com>
2026-04-15 14:43:26 +0530

    connected frontend to deployed backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-baseURL: "http://localhost:5000/api"});
+baseURL: "https://taskflow-x7iz.onrender.com/api"
+});
 
 // Add token automatically
 API.interceptors.request.use((req) => {
